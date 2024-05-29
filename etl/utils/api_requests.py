0da033301411878a96d183a87562dea11dc47971
alextgoldstein <ag52155@bose.com>
2024-05-29 16:02:45 -0300

    Enhancing Docstring

--- etl/utils/api_requests.py
+++ etl/utils/api_requests.py
@@ -4,16 +4,14 @@ from pandas import json_normalize
 
 
 class APIRequests:
-    # Constructor de la clase
     def __init__(self, base_url, flights_api_filters):
         self.base_url = base_url
         self.flights_api_filters = flights_api_filters
 
-    # Método de la clase
     def getFlightsData(self):
         """
-
-        :return:
+        Retrieves data from API in loop based on total records and 100 records limit
+        :return pd.Dataframe: API response:
         """
         offset = 0
         total_records = None
@@ -23,26 +21,31 @@ class APIRequests:
             response = requests.get(self.base_url, filters).json()
             if response.get('error'):
                 print(response)
-                # TODO: add continue so that this error is notified
                 break
 
+            # Returns limitation of API - Default: 100
             limit = response.get('pagination', {}).get('limit', 100)
             if total_records is None:
                 total_records = response.get('pagination', {}).get('total', 0)
 
+            # Expands columns under father tags to the same level joining with '_' for the name
             data = response.get('data')
             if data:
                 df = json_normalize(data, sep='_')
+                # Concatenates with general Dataframe to get all active flights
                 all_data_df = pd.concat([all_data_df, df], ignore_index=True)
 
             offset += limit + 1
             filters['offset'] = offset
-
             if offset >= total_records:
                 break
 
         return all_data_df
 
     def testAPI(self):
+        """
+        Single call to just return the minimum constraint records (100)
+        :return pd.Dataframe: API response
+        """
         data = requests.get(self.base_url, self.flights_api_filters).json()
         return json_normalize(data['data'], sep='_')
